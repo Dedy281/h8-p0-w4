d478f967cff03e564ff79383cd0c8856e382292a
Dedy <dedypermadi28@gmail.com>
2018-06-06 19:41:32 +0700

    no 1-3

---
+++ Exercises2.js
@@ -0,0 +1,15 @@
+function fpb(angka1, angka2){
+    var print = '';
+      for(var i = 1; i<=100; i++){
+        if(angka1%i==0 && angka2%i==0){
+            print = i;
+        }
+      }
+      return print;
+    }
+  
+  console.log(fpb(12, 16)); // 4
+  console.log(fpb(50, 40)); // 10
+  console.log(fpb(22, 99)); // 11
+  console.log(fpb(24, 36)); // 12
+  console.log(fpb(17, 23)); // 1
